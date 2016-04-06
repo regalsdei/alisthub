c03ff8c81ba6d4cd577f954b58a6f84d2c4fa205
Deepak khokhar <khokhard@smartdatainc.net>
2016-04-06 18:06:53 +0530

    update login and signup processes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -17,8 +17,43 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/signup',
             templateUrl: 'modules/authentication/views/signup.html'
         })
+         //Events screen=================================
+        .state('events', {
+            url: '/events',
+            templateUrl: 'modules/events/views/home.html'
+        })
         
         
+}).run(['$rootScope', '$location','$state', function($rootScope,$location, $state) {
+    //To add class
+    
+    $rootScope.menu=$rootScope.after_login_footer_div=true;
+    $rootScope.footer_login_div=false;
+    $rootScope.logout=function(){
+        $rootScope.isuserloggedIn=$rootScope.footer_login_div=false;
+        $rootScope.menu=$rootScope.after_login_footer_div=true;
+        $state.go('login');
+    }
+    }]).controller('loginController',function($scope,$rootScope,$location, $state){
+    $rootScope.class_status=1; 
+    $scope.login=function(){
+        $rootScope.isuserloggedIn=$rootScope.footer_login_div=true;
+        $rootScope.menu=$rootScope.after_login_footer_div=false;
+        $rootScope.class_status = 0;
+        $state.go('events');
+    }
+    
+    
+    }).controller('eventhomeController', function($scope) {
+  
+    if(window.innerWidth>767){ 
+    $scope.navCollapsed = false;	  
+    }else{
+    $scope.navCollapsed = true;
+    $scope.toggleMenu = function() {
+    $scope.navCollapsed = $scope.navCollapsed === false ? true: false;
+    };	  
+ }
 });
 
 
